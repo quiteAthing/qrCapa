3c0160214592e9668b1e72c46adbb346055a1487
J004G2-H <this@isnotreal.com>
2017-05-03 12:27:05 +0800

    myservlet

--- src/main/java/servlet/BinaryCounter.java
+++ src/main/java/servlet/BinaryCounter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet2",
+        name = "MyServlet",
         urlPatterns = {"/binaryCounter"}
     )
 public class BinaryCounter extends HttpServlet {
--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet",
+        name = "MyServlet2",
         urlPatterns = {"/hello"}
     )
 public class HelloServlet extends HttpServlet {
